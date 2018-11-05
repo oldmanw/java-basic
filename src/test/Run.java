a89d22f7a0b16a0a2db78ffadb8243c0a2ff42ae
pangzhihui01 <pangzhihui01@baidu.com>
2018-11-05 10:13:01 +0800

    添加test

---
+++ src/test/Run.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Run {
+    public void f() {
+        Test ex = new Test("Good");
+        MyTest d = new MyTest();
+    }
+}
---
+++ src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+
+    String str;
+
+    public Test() {
+        str = "Test";
+    }
+
+    public Test(String s) {
+        str = s;
+    }
+
+}
